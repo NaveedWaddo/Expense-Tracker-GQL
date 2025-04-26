024302b0fceefc4e50a47178f2df6ce0f048baa1
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-04-26 20:52:00 +0530

    mongodb model and connection

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,54 @@
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 
 import mergeResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { connectDB } from "./db/connectDB.js";
+dotenv.config();
 
-const server = new ApolloServer({
-  typeDefs: mergedTypeDefs,
-  resolvers: mergeResolvers,
+async function bootstrap() {
+  // 1) Connect to Mongo
+  await connectDB();
+
+  // 2) Set up Express + middleware
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  const httpServer = http.createServer(app);
+
+  // 3) Create & start ApolloServer
+  const server = new ApolloServer({
+    typeDefs: mergedTypeDefs,
+    resolvers: mergeResolvers,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+
+  // 4) Mount GraphQL (and Sandbox) at root
+  app.use(
+    "/",
+    expressMiddleware(server, {
+      context: ({ req }) => ({ req }),
+    })
+  );
+
+  // 5) Listen
+  await new Promise((res) => httpServer.listen({ port: 4000 }, res));
+  console.log("🚀 Server ready at http://localhost:4000/");
+}
+
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
-
-const { url } = await startStandaloneServer(server);
-
-console.log(`Server ready at ${url}`);
---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import passport from "passport";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    passport: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
