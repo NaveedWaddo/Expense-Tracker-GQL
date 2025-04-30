0b97d3e43c583892b99866d1dff11bda5b4d4994
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-04-30 22:28:40 +0530

    login signup transaction ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import TransactionPage from "./pages/TransactionPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Header from "./components/ui/Header.jsx";
 
-const App = () => {
-  return <div className="text-red-500">App</div>;
-};
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
