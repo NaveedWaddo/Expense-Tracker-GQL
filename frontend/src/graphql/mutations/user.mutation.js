2c8c66dbde0fa04543492d52d6a34f39216d1e42
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-05-02 01:00:58 +0530

    write all graphql query

--- frontend/src/graphql/mutations/user.mutation.js
+++ frontend/src/graphql/mutations/user.mutation.js
@@ -2,10 +2,28 @@ import { gql } from "@apollo/client";
 
 export const SIGN_UP = gql`
   mutation SignUp($input: SignUpInput!) {
-    signup(input: $input) {
+    signUp(input: $input) {
       _id
       name
       username
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      _id
+      name
+      username
+    }
+  }
+`;
+
+export const LOGOUT = gql`
+  mutation Logout {
+    logout {
+      message
     }
   }
 `;
